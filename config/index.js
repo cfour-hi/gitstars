8dd16ac7c0a34014b6589ad81a6fb8af9972cd25
monine <bob.zhouxiang@gmail.com>
2017-10-26 12:08:11 +0800

    修改资源公共路径

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     index: path.resolve(__dirname, '../index.html'),
     assetsRoot: path.resolve(__dirname, '../'),
     assetsSubDirectory: 'assets',
-    assetsPublicPath: 'monine.github.io/gitstars/',
+    assetsPublicPath: '/gitstars/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
