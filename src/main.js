cffec26394469ff216342d4a3b244e5951382d1f
monine <bob.zhouxiang@gmail.com>
2017-10-26 14:53:36 +0800

    授权

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 
 import { parseURLSearch } from './util'
 
@@ -11,21 +12,52 @@ if (process.env.NODE_ENV === 'development') {
   require('normalize.css')
 }
 
-let authState = window.localStorage.getItem('auth_state')
-if (!authState) {
-  authState = Date.now()
-  window.localStorage.setItem('auth_state', authState)
-  window.location.href = 'https://github.com/login/oauth/authorize?client_id=75cf00b02deb33e63424&state=' + authState
-} else {
-  const { state } = parseURLSearch()
-  console.log(`authState" ${authState}`)
-  console.log(`state" ${state}`)
+const clientId = '75cf00b02deb33e63424'
+const clientSecret = '6fa564cbd46f6bdfa1fb81ddce5503dcbe4ab4c4'
+const _accessToken = window.localStorage.getItem('access_token')
 
-  if (authState === state) {
-    console.log('已授权并且 state 相等')
+if (!_accessToken) {
+  const _code = window.localStorage.getItem('code')
+  if (!_code) {
+    const href = 'https://github.com/login/oauth/authorize'
+
+    let _authState = window.localStorage.getItem('auth_state')
+    if (!_authState) {
+      _authState = Date.now()
+      window.localStorage.setItem('auth_state', _authState)
+      window.location.href = `${href}?$client_id=${clientId}&state=${_authState}`
+    }
+
+    const { code } = parseURLSearch()
+    if (code) {
+      window.localStorage.setItem('code', code)
+
+      axios.post('https://github.com/login/oauth/access_token', {
+        code,
+        client_id: clientId,
+        client_secret: clientSecret
+      }).then(({ access_token }) => {
+        console.log(access_token)
+        window.localStorage.setItem('access_token', access_token)
+      })
+    } else {
+      console.warn('没有 code')
+    }
   } else {
-    console.log('已授权但 state 不相等')
+    window.localStorage.setItem('code', code)
+
+    axios.post('https://github.com/login/oauth/access_token', {
+      code: _code,
+      client_id: clientId,
+      client_secret: clientSecret
+    }).then(({ access_token }) => {
+      console.log(access_token)
+      window.localStorage.setItem('access_token', access_token)
+      window.accessToken = accessToken
+    })
   }
+} else {
+  window.accessToken = _accessToken
 }
 
 /* eslint-disable no-new */
--- src/util.js
+++ src/util.js
@@ -1,6 +1,10 @@
 export function parseURLSearch () {
   const search = {}
-  const kvs = window.location.search.slice(1).split('&')
+  const searchStr = window.location.search
+
+  if (!searchStr.length) return search
+
+  const kvs = searchStr.slice(1).split('&')
   for (const kv of kvs) {
     const skv = kv.split('=')
     search[skv[0]] = decodeURIComponent(skv[1])
