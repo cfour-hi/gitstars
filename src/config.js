9e8d37da6176d39f1fd7d480d9eb999a439898f2
monine <bob.zhouxiang@gmail.com>
2017-11-14 14:58:11 +0800

    optimize 开发环境与正式环境 OAuth App 配置切换

--- src/config.js
+++ src/config.js
@@ -1,6 +1,19 @@
+const config = {
+  development: {
+    clientId: 'bf9d4f20def51dbd5c5c',
+    clientSecret: 'e8f5bc4369f64b05d052819047ca9d5e1d8b2e8f'
+  },
+  production: {
+    clientId: '75cf00b02deb33e63424',
+    clientSecret: '6fa564cbd46f6bdfa1fb81ddce5503dcbe4ab4c4'
+  }
+}
+
+const { clientId, clientSecret } = config[process.env.NODE_ENV]
+
 export default {
-  clientId: '75cf00b02deb33e63424',
-  clientSecret: '6fa564cbd46f6bdfa1fb81ddce5503dcbe4ab4c4',
+  clientId,
+  clientSecret,
   filename: 'gitstars.json',
   description: `github stars manager for ${process.env.NODE_ENV}`
 }
