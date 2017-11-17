05eaa10eaf3291d52c1954c9da0022c78daf6e59
monine <bob.zhouxiang@gmail.com>
2017-11-17 16:06:51 +0800

    优化代码

--- src/config.js
+++ src/config.js
@@ -1,15 +1,6 @@
-const config = {
-  development: {
-    clientId: 'bf9d4f20def51dbd5c5c',
-    clientSecret: 'e8f5bc4369f64b05d052819047ca9d5e1d8b2e8f'
-  },
-  production: {
-    clientId: '75cf00b02deb33e63424',
-    clientSecret: '6fa564cbd46f6bdfa1fb81ddce5503dcbe4ab4c4'
-  }
-}
-
-const { clientId, clientSecret } = config[process.env.NODE_ENV]
+const isProduction = process.env.NODE_ENV === 'production'
+const clientId = isProduction ? '75cf00b02deb33e63424' : 'bf9d4f20def51dbd5c5c'
+const clientSecret = isProduction ? '6fa564cbd46f6bdfa1fb81ddce5503dcbe4ab4c4' : 'e8f5bc4369f64b05d052819047ca9d5e1d8b2e8f'
 
 export default {
   clientId,
