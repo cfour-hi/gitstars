4c5241266d3941fcf1e01f3318e1bb03b96621d3
Monine <monine.zhou@gmail.com>
2018-09-01 20:59:27 +0800

    update: lock dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'babel-polyfill'
-import '@/polyfill'
 import Vue from 'vue'
 import store from '@/store'
 import '@/element-ui'
