19f8259ddcb250e18c2d8907974219495a83803e
rayepeng <rayepeng@tencent.com>
2023-07-22 15:20:57 +0800

    fix: 🐛 fix code

--- api/oauth/access_token.js
+++ api/oauth/access_token.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 import express from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 const app = express();
 
-// 使用 body-parser middleware 解析 JSON 请求体
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.post('/api/oauth/access_token', async (req, res) => {
   const code = req.body.code;
   const client_id = req.body.client_id;
-  console.log(code, client_id);
-  // 从环境变量或其他安全存储中获取 client_secret
-  const client_secret = process.env.CLIENT_SECRET;
-  console.log(client_secret);
+  const client_secret = process.env.VITE_GITSTARS_CLIENT_SECRET;
   try {
     const response = await axios.post('https://github.com/login/oauth/access_token', {
       client_id,
