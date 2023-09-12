6de42a8d1c0eab8321b063d0ad6f7fc87756af86
cfour <cfour.zhou@gmail.com>
2023-09-12 17:45:03 +0800

    chore: vercel edge function test

--- api/oauth/access_token.js
+++ api/oauth/access_token.js
@@ -23,8 +23,8 @@ async function toJSON(body) {
 
 async function allowCors(fn) {
   return async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Methods',
       'GET,OPTIONS,PATCH,DELETE,POST,PUT',
