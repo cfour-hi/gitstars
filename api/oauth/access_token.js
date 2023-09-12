ee134569eebe4f59f9c36e6a97475ebad970dd85
cfour <cfour.zhou@gmail.com>
2023-09-12 15:45:05 +0800

    chore: vercel cors

--- api/oauth/access_token.js
+++ api/oauth/access_token.js
@@ -17,7 +17,29 @@ async function toJSON(body) {
   return read();
 }
 
-export default async (request) => {
+async function allowCors(fn) {
+  return async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
+}
+
+async function handler(request) {
   const requestBodyJson = await toJSON(request.body);
   try {
     const res = await fetch('https://github.com/login/oauth/access_token', {
@@ -38,4 +60,6 @@ export default async (request) => {
     console.error(e);
     return new Response(e.message);
   }
-};
+}
+
+export default allowCors(handler);
