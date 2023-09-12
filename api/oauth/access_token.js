f8b8166d8ece7541a1015b7a7dfd63aa8d03d2d6
cfour <cfour.zhou@gmail.com>
2023-09-12 18:32:54 +0800

    chore: update vercel api

--- api/oauth/access_token.js
+++ api/oauth/access_token.js
@@ -5,6 +5,7 @@ export const config = {
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'OPTIONS,POST',
+  'Access-Control-Allow-Headers': 'Content-Type',
 };
 
 export default async (request, context) => {
