6f26dd84b9918d427023dbfe03046da147cfda09
cfour <cfour.zhou@gmail.com>
2023-09-12 16:44:40 +0800

    chore: vercel serverless api test

--- api/oauth/access_token.js
+++ api/oauth/access_token.js
@@ -1,26 +1,3 @@
-export const config = {
-  runtime: 'edge',
-};
-
-async function toJSON(body) {
-  const reader = body.getReader();
-  const decoder = new TextDecoder();
-  const chunks = [];
-
-  async function read() {
-    const { done, value } = await reader.read();
-    if (done) {
-      return JSON.parse(chunks.join(''));
-    }
-
-    const chunk = decoder.decode(value, { stream: true });
-    chunks.push(chunk);
-    return read();
-  }
-
-  return read();
-}
-
 async function allowCors(fn) {
   return async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -37,20 +14,18 @@ async function allowCors(fn) {
       res.status(200).end();
       return;
     }
-    console.log('allowCors req', req.body);
     return await fn(req, res);
   };
 }
 
 async function handler(request) {
   console.log('handler request', request.body);
-  const requestBodyJson = await toJSON(request.body);
   try {
     const res = await fetch('https://github.com/login/oauth/access_token', {
       method: 'POST',
       body: JSON.stringify({
-        code: requestBodyJson.code,
-        client_id: requestBodyJson.client_id,
+        code: request.body.code,
+        client_id: request.body.client_id,
         client_secret: process.env.VITE_GITSTARS_CLIENT_SECRET,
       }),
       headers: {
