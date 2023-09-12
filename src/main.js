bbe1ed806db3cab0c9695343d6d824d96ffb8536
cfour <cfour.zhou@gmail.com>
2023-09-12 18:23:58 +0800

    feat: github  auth by vercel

--- src/main.js
+++ src/main.js
@@ -39,12 +39,6 @@ async function resolveToken() {
     onAppError(err);
   });
 
-  // {
-  //   "error": "bad_verification_code",
-  //   "error_description": "The code passed is incorrect or expired.",
-  //   "error_uri": "/apps/managing-oauth-apps/troubleshooting-oauth-app-access-token-request-errors/#bad-verification-code"
-  // }
-
   if (!res.access_token) {
     onAppError({ message: `${res.error}. ${res.error_description}` });
     throw new Error(res);
--- src/server/gitstars.js
+++ src/server/gitstars.js
@@ -1,7 +1,11 @@
 import { httpRequestGitstars } from './http-request';
 
 export async function getToken(code) {
-  return await httpRequestGitstars.post('/api/oauth/access_token', {
+  const url = import.meta.env.DEV
+    ? '/api/oauth/access_token'
+    : 'https://gitstars-vercel.cfour.top/api/oauth/access_token';
+
+  return await httpRequestGitstars.post(url, {
     code,
     client_id: import.meta.env.VITE_GITSTARS_CLIENT_ID,
   });
