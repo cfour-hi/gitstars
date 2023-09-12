d4717d44cd56018cc603d4891d5a295724a80c92
cfour <cfour.zhou@gmail.com>
2023-09-12 17:40:04 +0800

    choare: vercel edge function test

--- api/oauth/access_token.js
+++ api/oauth/access_token.js
@@ -1,3 +1,26 @@
+export const config = {
+  runtime: 'edge',
+};
+
+async function toJSON(body) {
+  const reader = body.getReader();
+  const decoder = new TextDecoder();
+  const chunks = [];
+
+  async function read() {
+    const { done, value } = await reader.read();
+    if (done) {
+      return JSON.parse(chunks.join(''));
+    }
+
+    const chunk = decoder.decode(value, { stream: true });
+    chunks.push(chunk);
+    return read();
+  }
+
+  return read();
+}
+
 async function allowCors(fn) {
   return async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -20,12 +43,13 @@ async function allowCors(fn) {
 
 async function handler(request) {
   console.log('handler request', request.body);
+  const requestBodyJson = await toJSON(request.body);
   try {
     const res = await fetch('https://github.com/login/oauth/access_token', {
       method: 'POST',
       body: JSON.stringify({
-        code: request.body.code,
-        client_id: request.body.client_id,
+        code: requestBodyJson.code,
+        client_id: requestBodyJson.client_id,
         client_secret: process.env.VITE_GITSTARS_CLIENT_SECRET,
       }),
       headers: {
