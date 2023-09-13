2bf1d04d04d92028fa8d9d5987ced3a49dfb2800
cfour <cfour.zhou@gmail.com>
2023-09-13 09:23:12 +0800

    chore: revert api

--- src/server/gitstars.js
+++ src/server/gitstars.js
@@ -1,11 +1,7 @@
 import { httpRequestGitstars } from './http-request';
 
 export async function getToken(code) {
-  const url = import.meta.env.DEV
-    ? '/api/oauth/access_token'
-    : 'https://gitstars-vercel.cfour.top/api/oauth/access_token';
-
-  return await httpRequestGitstars.post(url, {
+  return await httpRequestGitstars.post('/api/oauth/access_token', {
     code,
     client_id: import.meta.env.VITE_GITSTARS_CLIENT_ID,
   });
